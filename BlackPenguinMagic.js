d163d3ab290f30c503c85f8e8e7cf98d5d386397
Joel <j.n.brewster@gmail.com>
2016-01-14 21:12:43 +0900

    Back home

---
+++ BlackPenguinMagic.js
@@ -0,0 +1,28 @@
+/*
+Create a Penguin object with the variable name penguin and any name you'd like.
+
+Then call penguin.sayName();.
+
+Then be amazed.
+*/
+// the original Animal class and sayName method
+function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+}
+Animal.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+// define a Penguin class
+function Penguin(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+
+// set its prototype to be a new instance of Animal
+Penguin.prototype = new Animal();
+
+var penguin = new Penguin();
+
+penguin.sayName();
