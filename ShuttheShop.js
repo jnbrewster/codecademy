155d7bf9e944e739e1c281ea1974207c4793fb3e
Joel <j.n.brewster@gmail.com>
2016-01-17 23:30:40 +0900

    cash register project

---
+++ ShuttheShop.js
@@ -0,0 +1,14 @@
+/*
+Instructions
+Create a new object called cashRegister with the property total initialized to 0.
+
+Then change the property total to 2.99 using dot notation.
+*/
+//Create the object called cashRegister
+//and initialize its total property
+var cashRegister = {
+    total: 0
+}
+
+//Using dot notation change the total property
+cashRegister.total = 2.99;
