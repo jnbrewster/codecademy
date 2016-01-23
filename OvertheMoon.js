1d1f018f56c56d2199ec607b3f2020c4df399643
Joel <j.n.brewster@gmail.com>
2016-01-23 12:33:49 +0900

    Add a new staff instance with an object constructor

---
+++ OvertheMoon.js
@@ -0,0 +1,18 @@
+/*
+Create an object constructor called StaffMember which takes two parameters—name and discountPercent. And then have the (public) properties name and discountPercent equal the parameters.
+
+To help, we have already created two employees using this constructor. Sally and Bob already have their staff discount set up: Sally getting 5% off and Bob getting 10%.
+
+Create a new instance of the object for yourself called me with your massive staff discount bonus of 20%.
+*/
+// create a constructor for the StaffMember class
+
+
+var sally = new StaffMember("Sally",5);
+var bob = new StaffMember("Bob",10);
+
+//Create a StaffMember for yourself called me
+var me = new StaffMember("Joel", 20);
+
+
+
